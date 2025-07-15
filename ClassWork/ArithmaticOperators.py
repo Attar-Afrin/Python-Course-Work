1e93a71fc059702d14dbdbab6304341731e45684
Attar-Afrin <attarafrin503@gmail.com>
2025-07-15 14:53:11 +0530

    Arithmatic

--- ClassWork/ArithmaticOperators.py
+++ ClassWork/ArithmaticOperators.py
@@ -1,5 +1,9 @@
 #ArithmaticOperators
 a=10
 b=20
-print("Addition of a+b",a+b)
+print("Addition of (a+b)",a+b)
 print("Subtraction of (a-b)",a-b)
+print("Multiplication of(a*b)",a*b)
+print("Division of (a/b)",a/b)
+print("Floor Division of(a//b)",a//b)
+print("Modulus of(a % b)",a %b)
