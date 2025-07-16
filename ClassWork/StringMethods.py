2cd53ff930edda3a944cf3a67f98199bb46f9747
Attar-Afrin <attarafrin503@gmail.com>
2025-07-16 20:01:45 +0530

    String

---
+++ ClassWork/StringMethods.py
@@ -0,0 +1,35 @@
+#1 Built-in String Functions
+text = "Hello World"
+print(len(text)) #11
+
+print(max("Python PRogramming")) #y
+print(min("JavaProgramming")) #J
+
+'''NOTE---->The Ascii value of a space is 32
+ ,value of 'A' is 65 and a is 97'''
+
+print(sorted("python")) #['h', 'n', 'o', 'p', 't', 'y']
+
+#Character to ascii
+print(ord('Z'))  #90
+
+#Character to ascii
+print(chr(49)) #1
+
+#2 Case Conversion Methods
+
+print("Attar Afrin".upper()) #ATTAR AFRIN
+print("Hello".lower()) #hello
+print("python".capitalize()) #Python
+print("hello world".title()) #Hello World
+print("PyThOn pRograMMing".swapcase()) #pYtHoN PrOGRAmmING
+print("ß".casefold()) #ss
+
+#3 Alignment & Formatting Methods
+print("python".center(10, "*")) #**python**
+print("py".ljust(5, "-") ) #py---
+print("py".rjust(5, "-")) #---py
+print("42".zfill(5)) #00042
+print("Name: {}, Age:{}".format("Tom", 25)) #Name: Tom, Age:25
+print("{name} is{age}".format_map({'name':'Tom', 'age': 25})) #Tom is25
+
