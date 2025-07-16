aaae9e38ee29992e10101144512c2a07c42d1313
Attar-Afrin <attarafrin503@gmail.com>
2025-07-16 14:55:09 +0530

    String

---
+++ ClassWork/Strings.py
@@ -0,0 +1,16 @@
+# Operations on Strings
+#1 Concatination
+str1 = "Hello"
+str2 = "World"
+result = str1 + " " + str2
+print(result)
+
+#2 Repetition
+print("Python " *3)
+print("[a]"*5)
+
+
+#3 Indexing
+Language="English"
+print(Language[5])
+print(Language[-1])
