f878a3ac1b569027c24cc279bdba68a76e825488
Attar-Afrin <attarafrin503@gmail.com>
2025-07-16 09:00:27 +0530

    Arithmatic

--- ClassWork/Operators.py
+++ ClassWork/Operators.py
@@ -1,4 +1,4 @@
-#ArithmaticOperators
+#1 ArithmaticOperators
 a=10
 b=20
 print("Addition of (a+b)",a+b) #Addition of (a+b) 30
@@ -7,3 +7,31 @@ print("Multiplication of(a*b)",a*b) #Multiplication of(a*b) 200
 print("Division of (a/b)",a/b) #Division of (a/b) 0.5
 print("Floor Division of(a//b)",a//b) #Floor Division of(a//b) 0
 print("Modulus of(a % b)",a %b) #Modulus of(a % b) 10
+
+
+#2 Comparison Operators
+x=20
+y=15
+print("Equal to",x==y) #Equal to False
+print("Not Equal to",x!=y) #Not Equal to True
+print("Greater than",x>y) #Greater than True
+print("Less than",x<y) #Less than False
+print("Greater than or Equal to",x>=y) #Greater than or Equal to True
+print("Less than or Equal to",x<=y) #Less than or Equal to False
+
+#3 Assignment Operators
+x=50
+x+=5
+print("Add & Assign",x) #Add & Assign 55
+x-=10
+print("Subtract & Assign ",x) #Subtract & Assign  45
+x*=5
+print("Multiply & Assign",x) #Multiply & Assign 225
+x/=2
+print("Divide & Assign",x) #Divide & Assign 112.5
+x//=2
+print("Floor Divide & Assign",x) #Floor Divide & Assign 56.0
+x%=5
+print("Modulus & Assign",x) #Modulus & Assign 1.0
+x**10
+print("Exponentiate & Assign",x) #Exponentiate & Assign 1.0
