e6079bd3001a4920a6bc9cfce82ffdc1c6365882
Attar-Afrin <attarafrin503@gmail.com>
2025-07-21 12:16:18 +0530

    Sets

--- ClassWork/Sets.py
+++ ClassWork/Sets.py
@@ -7,3 +7,17 @@ print(my_set)
 print(3 in my_set) #True
 print(6 not in my_set) #True
 
+#b. Union Method
+set1={1,2,4,56,7}
+set2={9,8,7,6,5}
+print(set1|set2) #{1, 2, 4, 5, 6, 7, 8, 9, 56}
+print(set1.union(set2)) #{1, 2, 4, 5, 6, 7, 8, 9, 56}
+
+#c. Intersection (& or intersection() method)
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+result = set1 & set2
+print(result)
+
+
+
