c395f10569ad2c1a02172488389db6de0a5bc0a3
Attar-Afrin <attarafrin503@gmail.com>
2025-07-24 14:38:29 +0530

    While

--- ClassWork/whileExample.py
+++ ClassWork/whileExample.py
@@ -7,6 +7,21 @@ while moves>0:
     print(f"{moves} left !You can play")
     moves-=1
 else:
-    print("Game Over")
+    print("Game Over") #Enter the winning point35
+# 40 left !You can play
+#39 left !You can play
+#38 left !You can play
+#37 left !You can play
+#36 left !You can play
+#Congrats ! You have won the game
+
+
+# Sum of the Numbers
+n=int(input("Enter the Number : "))
+s=0
+while(n>0):
+    s+=(n%10)
+    n//=10
+print(s)
 
 
