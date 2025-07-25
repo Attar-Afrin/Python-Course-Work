19cf925399f8c9e74cd490cb419f6f7677f49a62
Attar-Afrin <attarafrin503@gmail.com>
2025-07-25 15:01:04 +0530

    Patterns

--- ClassWork/Patterns.py
+++ ClassWork/Patterns.py
@@ -48,6 +48,9 @@ for row in range(n):
 *
 * *
 * * *
+* * * *
+* * * * *
+* * * * * *
 '''
 
 n=int(input("Enter n value"))
@@ -55,4 +58,149 @@ for row in range(n):
     for col in range(row):
         print("*",end=" ")
     print()
+
+'''
+* * * * * * 
+* * * * *
+* * * *
+* * *
+* *
+*
+'''
+
+n=int(input("Enter n value"))
+for row in range(n):
+    for col in range(n-row):
+        print("*",end=" ")
+    print()
+
+n=int(input("Enter n value"))
+for row in range(n):
+    for col in range(n-row-1):
+        print(" ",end=" ")
+    for col in range(row):
+        print("*",end=" ")
+    print()
+'''
+* * * * * * * 
+  * * * * * *
+    * * * * *
+      * * * *
+        * * *
+          * *
+            *
+'''
+
+n=int(input("Enter n value"))
+for row in range(n):
+    for col in range(row):
+        print(" ",end=" ")
+    for col in range(n-row):
+        print("*",end=" ")
+    print()
+
+'''
+* 
+* *
+* * *
+* * * *
+* * * * *
+* * * * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+'''
+
+n=int(input("Enter n value"))
+for row in range(n):
+    if row<=n//2:
+    
+        for col in range(row+1):
+            print("*",end=" ")
+    else:
+
+        for col2 in range(n-row):
+
+            print("*",end=" ")
+    print()
+
+'''
+* * * * * * * 
+*           *
+*           *
+*           *
+*           *
+*           *
+* * * * * * *
+'''
+
+n=int(input("Enter n value for square pattern"))
+for row in range(n):
+    for col in range(n):
+        if row==0 or row==n-1 or col==0 or col==n-1:
+            print("*",end=" ")
+        else:
+
+            print(" ",end=" ")
+    print()
+
+'''
+* * * * * * * 
+*           *
+*           *
+* * * * * * *
+*           *
+*           *
+* * * * * * *
+'''
+n=int(input("Enter n value for square pattern"))
+for row in range(n):
+    for col in range(n):
+        if row==0 or row==n-1 or col==0 or col==n-1 or row==n//2:
+            print("*",end=" ")
+        else:
+
+            print(" ",end=" ")
+    print()
+
+'''
+* * * * * * * * 
+            *
+          *
+        *
+      *
+    *
+  *
+* * * * * * * *
+'''
+n=int(input("Enter n value for z---pattern"))
+for row in range(n):
+    for col in range(n):
+        if row==0 or row==n-1 or col+row==n-1:
+            print("*",end=" ")
+        else:
+
+            print(" ",end=" ")
+    print()
+
+n=int(input("Enter n value for z---pattern"))
+for row in range(n):
+    for col in range(n):
+        if row==0 or row==n-1 or col+row==n-1 or row==col:
+            print("*",end=" ")
+        else:
+
+            print(" ",end=" ")
+    print()
+
+
+
+
+
+
+
+
+
 
