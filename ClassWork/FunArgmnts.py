9c04b3c4033646efa03706e66efb8bf03e055934
Attar-Afrin <attarafrin503@gmail.com>
2025-08-02 14:46:37 +0530

    Arguments

--- ClassWork/FunArgmnts.py
+++ ClassWork/FunArgmnts.py
@@ -1,5 +1,5 @@
 #1 Positional Arguments
-def stu_names(name,rollno,marks,grades,course):
+'''def stu_names(name,rollno,marks,grades,course):
     print(name,rollno,marks,grades,course)
 
 name=input("Name :")
@@ -8,5 +8,56 @@ marks=input("Marks: ")
 grades=input("Grades :")
 course=input("Coures:")
 stu_names(name,rollno,marks,grades,course)
-stu_names(rollno,marks,grades,course,name)
+'''
+'''Name :Afrin
+Roll No503
+Marks: 100
+Grades :A
+Coures:python
+Afrin 503 100 A python
+'''
+'''stu_names(rollno,marks,grades,course,name)
 # Note===positional arguments takes the arguments in oreder wise we can give values statically
+'''
+
+#2 Keyword Arguments
+def student_details(name,rollno,marks,grade,course):
+    print('Name:',name)
+    print('Rollno:',rollno)
+    print('Marks:',marks)
+    print('Grade:',grade)
+    print('Course:',course)
+
+name=input("Name: ")
+rollno=int(input("Roll no: "))
+marks=int(input("Marks: "))
+grade=input("Grade: ")
+student_course=input("Course: ")
+
+student_details(name=name,rollno=rollno,marks=marks,grade=grade,course=student_course)
+
+student_details(rollno=rollno,name=name,grade=grade,course=student_course,marks=marks)
+
+student_details(rollno=56,name='ramya',grade='A',course='Mysql',marks=99)
+'''
+Name: Afreen
+Roll no: 23
+Marks: 432
+Grade: a
+Course: python
+Name: Afreen
+Rollno: 23
+Marks: 432
+Grade: a
+Course: python
+Name: Afreen
+Rollno: 23
+Marks: 432
+Grade: a
+Course: python
+Name: ramya
+Rollno: 56
+Marks: 99
+Grade: A
+Course: Mysql
+'''
