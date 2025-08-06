f0d5369922b6401cad30e459d429b18d4700c3f1
Attar-Afrin <attarafrin503@gmail.com>
2025-08-06 14:11:16 +0530

    Lambda

---
+++ ClassWork/lambda.py
@@ -0,0 +1,9 @@
+#Lambda
+#Syntax--->fun name=lambda parameters
+#sumof three numbers
+sumofthree=lambda a,b,c:a+b+c
+print(sumofthree(12,13,14)) #39
+
+#even oddchecking
+evenOdd=lambda n:"Even" if n%2==0 else "Odd"
+print(evenOdd(100))#Even
