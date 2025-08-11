e289488921a63d84d5f97ee66f3dfa2185098331
Attar-Afrin <attarafrin503@gmail.com>
2025-08-11 15:36:07 +0530

    python_weekly_test

--- PractiseWork/weeklyTest3.py
+++ PractiseWork/weeklyTest3.py
@@ -9,4 +9,9 @@ print(fun([1,2,3,4,6,8]))
 def create_email(first,last,domain):
     update_str=print(f"{first}.{last}@{domain}.com")
     return update_str
-print(create_email('afreen','attar','gmail'))
+create_email('afreen','attar','gmail')
+
+def format_invoice(item,quantity,price):
+    frmt_str=print(f"{item}*{quantity}@$ {price} = ${quantity*price}")
+    return frmt_str
+print(format_invoice('pen',3,10))
